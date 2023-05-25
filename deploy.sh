8d5730c3e75e7bd4bb9c0fc860f6e5bc6870059f
chenshiyan <917244295@qq.com>
2023-05-25 10:46:13 +0800

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,11 +4,11 @@ set -e
 
 npm run docs:build
 
-# cd docs/.vitepress/dist
+cd docs/.vitepress/dist
 git init
 git add -A
 git commit -m "自动部署"
-git push -f https://github.com/cyan0714/knowleage-lib.git master:gh-website
+git push -f https://github.com/cyan0714/knowleage-lib.git master:gh-web
 
 cd -
-# rm -rf docs/.vitepress/dist
+rm -rf docs/.vitepress/dist
