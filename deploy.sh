f9c5ebf15a072c67c712d92670b0eb5d2707d039
chenshiyan <917244295@qq.com>
2023-05-25 10:29:18 +0800

    自动部署

--- deploy.sh
+++ deploy.sh
@@ -5,10 +5,10 @@ set -e
 npm run docs:build
 
 # cd docs/.vitepress/dist
-
-git add docs/.vitepress/dist/
+git init
+git add -A
 git commit -m "自动部署"
-git push -f https://github.com/cyan0714/knowleage-lib.git master:gh-docs
+git push -f https://github.com/cyan0714/knowleage-lib.git master:gh-personal
 
 cd -
 # rm -rf docs/.vitepress/dist
