f30d29e3113ad43969498d717e278a8b5dbb46fd
Cyan <917244295@qq.com>
2024-02-29 11:20:58 +0800

    change deploy config

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ cd docs/.vitepress/dist
 git init
 git add -A
 git commit -m "自动部署"
-git push -f https://github.com/cyan0714/vitepress-blog.git master:master
+git push -f https://github.com/cyan0714/vitepress-blog.git master:gh-web
 
 cd -
 rm -rf docs/.vitepress/dist
